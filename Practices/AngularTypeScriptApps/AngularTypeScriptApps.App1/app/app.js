4bd4d245d7381c0a18db2242ab2663d41b40912b
Foyzul Karim <foyzulkarim@gmail.com>
2015-10-13 02:06:34 +0600

    video 1 of typescript

---
+++ Practices/AngularTypeScriptApps/AngularTypeScriptApps.App1/app/app.js
@@ -0,0 +1,24 @@
+var App;
+(function (App) {
+    // OOP mental context
+    var Config = (function () {
+        function Config() {
+            console.log('I am in config constructor.');
+        }
+        return Config;
+    })();
+    var HomeController = (function () {
+        function HomeController() {
+            console.log('I am in home controller constructor.');
+        }
+        HomeController.prototype.clickme = function () {
+            console.log('I am in home controller click me method.');
+        };
+        return HomeController;
+    })();
+    App.HomeController = HomeController;
+    // angular mental context
+    angular.module('app', []);
+    angular.module('app').config(Config);
+    angular.module('app').controller('HomeController', HomeController);
+})(App || (App = {}));
---
+++ Practices/AngularTypeScriptApps/AngularTypeScriptApps.App1/app/app.ts
@@ -0,0 +1,24 @@
+﻿module App {
+    // OOP mental context
+    class Config {
+        constructor() {
+            console.log('I am in config constructor.');
+        }
+    }
+
+    export class HomeController {
+        constructor() {
+            console.log('I am in home controller constructor.');            
+        }
+
+        clickme(): void {
+            console.log('I am in home controller click me method.');                        
+        }
+    }
+
+
+    // angular mental context
+    angular.module('app', []);
+    angular.module('app').config(Config);
+    angular.module('app').controller('HomeController', HomeController);
+}
