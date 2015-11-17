bdc75e40a338b2ad04ef7fed9be3611a73d0d672
Foyzul Karim <foyzulkarim@gmail.com>
2015-11-17 23:20:31 +0600

    initial commit

---
+++ AdvancedCourse/Level1/ZooApp/ZooApp.Models/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooApp.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Food { get; set; }
+        public string Origin { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ AdvancedCourse/Level1/ZooApp/ZooApp.Models/ZooContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooApp.Models
+{
+    public class ZooContext : DbContext
+    {
+        public ZooContext() : base("ZooContext")
+        {
+            
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
