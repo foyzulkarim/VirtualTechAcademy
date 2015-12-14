0853511b8334888ab2033a23d65a16b4845dc6db
Foyzul Karim <foyzulkarim@gmail.com>
2015-12-15 00:12:05 +0600

    built in auth mechanism test

---
+++ AdvancedCourse/Level2/Auth/MvcAppWithAuth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcAppWithAuth.MvcApplication" Language="C#" %>
---
+++ AdvancedCourse/Level2/Auth/MvcAppWithAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcAppWithAuth.Startup))]
+namespace MvcAppWithAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
